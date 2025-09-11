395e9a6283c3b3a6f1805a373e2ca80c861f80ff
Richard Hu <richard.hu@technexion.com>
2025-09-11 16:12:44 +0800

    main: ddr: add weak BOARD_DetectDDR implementation
    This allows boards to implement custom DDR detection logic while keeping
    the default behavior unchanged for existing boards.

--- oei/ddr/main.c
+++ oei/ddr/main.c
@@ -61,6 +61,11 @@ static uint32_t Ddr_Load_Training_Data(uint32_t offset)
     return (size == lsize ? ROM_API_OKAY : ROM_API_ERR_INV_PAR);
 }
 
+__WEAK void BOARD_DetectDDR(void)
+{
+    /* Empty weak implementation */
+}
+
 int oei_main(uint32_t argc, uint32_t *argv)
 {
     int ret = 0;
@@ -75,6 +80,7 @@ int oei_main(uint32_t argc, uint32_t *argv)
 #if !defined(DEBUG)
     ts = SYSCTR_GetUsec64();
 #endif
+    BOARD_DetectDDR();
 
     /* Board specific hardware initialization */
     BOARD_InitHardware();
